ab2d0d22edf2c1566385ff9ff9f055b1b4ecfced
beistvan <iberegsz@student.42vienna.com>
2024-09-27 16:12:12 +0200

    Create compare_cpp00_ex02_logs.sh
    Comparing two logs: one from running the account and one from the subject.

---
+++ compare_cpp00_ex02_logs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Comparing two logs: one from running the account and one from the subject."
+
+account_log=$(./account | cut -d' ' -f2-)
+subject_log=$(cat 19920104_091532.log | cut -d' ' -f2-)
+
+diff <(echo $account_log) <(echo $subject_log)
+if [ $? -eq 0 ]; then
+    echo "The logs are identical."
+else
+    echo "The logs are different."
+fi
